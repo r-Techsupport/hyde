f174417c080fb3d3f9fe947f00bdd460c76fe737
zleyyij <zleyyij@noreply.users.github.com>
2024-05-16 19:26:44 -0600

    feat: removed unneeded assets

--- frontend/src/routes/editor/+page.svelte
+++ frontend/src/routes/editor/+page.svelte
@@ -32,11 +32,11 @@
     <div style="display: flex;flex-direction: column;">
         <TopBar/>
         <div class="editor-controls">
-            <!-- <img src="/assets/save.svg" alt="Save your work" width="40" height="40"/> -->
+            <!-- Cancel -->
             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px">
                 <path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/>
             </svg>
-            <!-- <img src="/assets/cancel.svg" alt="Cancel changes" width="40" height="40"/> -->
+            <!-- Save -->
             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px">
                 <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
             </svg>
