8a65c5c4a6c0da350b4b6be73546f5eca9eadd03
zleyyij <zleyyij@noreply.users.github.com>
2024-05-20 14:10:08 -0600

    feat: cli config

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,28 @@
 mod handlers;
 
 use axum::{routing::get, Router};
-use log::info;
+use clap::{Parser, builder::{PossibleValuesParser, TypedValueParser}};
+use log::{info, LevelFilter};
 use tower_http::{normalize_path::NormalizePathLayer, services::ServeDir};
 use std::env::current_exe;
 
-// TODO: configure basic clap set up for command line config, you can
-// use hwapi as a reference.
+#[derive(Parser)]
+struct Args {
+    #[arg(short = 'p', long = "port", default_value_t = String::from("8080"))]
+    port: String,
+    #[arg(
+        short = 'v',
+        long = "verbosity",
+        default_value_t = LevelFilter::Info,
+        value_parser = PossibleValuesParser::new(["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF"])
+            .map(|s| s.to_lowercase().parse::<LevelFilter>().unwrap())
+    )]
+    logging_level: LevelFilter
+}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    let cli_args = Args::parse();
     // Initialize logging
     env_logger::builder()
         .filter(None, log::LevelFilter::Info)
@@ -28,9 +41,8 @@ async fn main() -> color_eyre::Result<()> {
         // Enable support for routes that have or don't have a trailing slash
         .layer(NormalizePathLayer::trim_trailing_slash());
 
-    let port = "3000";
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
-    info!("Application starting, listening on port: {port}");
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", cli_args.port)).await?;
+    info!("Application starting, listening on port: {}", cli_args.port);
     axum::serve(listener, app).await?;
 
     Ok(())
