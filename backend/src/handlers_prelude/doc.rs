3c789a227b58953adf59ab7e10f68d6c83546361
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-06-29 12:02:27 +0300

    Move permission checking out of handler to mod.rs public function

--- backend/src/handlers_prelude/doc.rs
+++ backend/src/handlers_prelude/doc.rs
@@ -1,17 +1,13 @@
-use std::collections::HashMap;
-
 use axum::{
     debug_handler,
     extract::{Query, State},
     http::{HeaderMap, StatusCode},
     Json,
 };
-use chrono::{DateTime, Utc};
-use color_eyre::eyre::{Context, ContextCompat};
-use log::{debug, error, trace, warn};
+use log::{error, warn};
 use serde::{Deserialize, Serialize};
 
-use crate::{db::User, eyre_to_axum_err, perms::Permission, AppState};
+use crate::{perms::Permission, require_perms, AppState};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct GetDocQuery {
@@ -61,59 +57,14 @@ pub async fn put_doc_handler(
     headers: HeaderMap,
     Json(body): Json<PutDocRequestBody>,
 ) -> Result<StatusCode, (StatusCode, String)> {
-    let maybe_user: Option<FoundUser> =
-        find_user(&state, headers).await.map_err(eyre_to_axum_err)?;
-    let author: User;
     // Unauthorized means that the client has not identified itself, where as forbidden
     // means that the client has identified itself, and it does not have the required permissions
-    match maybe_user {
-        Some(user) => match user {
-            FoundUser::ExpiredUser(u) => {
-                return Err((
-                    StatusCode::UNAUTHORIZED,
-                    format!(
-                    "The access token has expired for the user {}, they must authenticate again.",
-                    u.username
-                ),
-                ))
-            }
-            FoundUser::User(u) => {
-                let groups = &state
-                    .db
-                    .get_user_groups(u.id)
-                    .await
-                    .map_err(eyre_to_axum_err)?;
-                let mut has_permission = false;
-                for group in groups {
-                    if state
-                        .db
-                        .group_has_permission(group.id, Permission::ManageContent)
-                        .await
-                        .map_err(eyre_to_axum_err)?
-                    {
-                        has_permission = true;
-                    }
-                }
-                if has_permission {
-                    author = u;
-                } else {
-                    return Err((
-                        StatusCode::FORBIDDEN,
-                        format!(
-                            "User {:?} lacks the permission to edit documents.",
-                            u.username
-                        ),
-                    ));
-                }
-            }
-        },
-        None => {
-            return Err((
-                StatusCode::UNAUTHORIZED,
-                "No valid user is authenticated.".to_string(),
-            ))
-        }
-    };
+    let author = require_perms(
+        axum::extract::State(&state),
+        headers,
+        &[Permission::ManageContent],
+    )
+    .await?;
 
     let gh_token = match &state.gh_credentials.get(&state.reqwest_client).await {
         Ok(t) => t.clone(),
@@ -140,47 +91,4 @@ pub async fn put_doc_handler(
             ))
         }
     }
-}
-
-// This stuff should probably be moved but I don't know where.
-/// The output of a find_user call, used to differentiate between expired users and valid users
-enum FoundUser {
-    ExpiredUser(User),
-    User(User),
-}
-
-/// Find the user attached to a particular request, if there is one, and the access token is still valid
-async fn find_user(state: &AppState, headers: HeaderMap) -> color_eyre::Result<Option<FoundUser>> {
-    let mut cookies: HashMap<&str, &str> = HashMap::new();
-    // There can be multiple cookie headers, and each cookie header can contain multiple cookies
-    let cookie_headers = headers.get_all("Cookie");
-    for cookie_header in cookie_headers {
-        let deserialized_cookie = cookie_header
-            .to_str()
-            .wrap_err("Cookie header contains invalid UTF-8")?;
-        for nv_pair in deserialized_cookie.split("; ") {
-            let (name, value) = nv_pair.split_once('=').wrap_err("Malformed cookie")?;
-            cookies.insert(name, value);
-        }
-    }
-    if let Some(token) = cookies.get("access-token") {
-        trace!("Request was made that contains an access-token cookie");
-        if let Some(user) = state.db.get_user_from_token(token.to_string()).await? {
-            let expiration_date = DateTime::parse_from_rfc3339(&user.expiration_date)
-                .wrap_err("Expiration time in database is not a valid time")?;
-            if expiration_date < Utc::now() {
-                debug!("User {:?} made a request that requires a valid access token but their access token expired", user.username);
-                return Ok(Some(FoundUser::ExpiredUser(user)));
-            } else {
-                debug!("User {:?} made a request that requires a valid access token and they have a valid access token", user.username);
-                return Ok(Some(FoundUser::User(user)));
-            }
-        } else {
-            trace!("No user was found in the database with the request's access token");
-        }
-    } else {
-        trace!("Request was made that lacked an access-token cookie");
-    }
-
-    Ok(None)
 }
--- backend/src/handlers_prelude/mod.rs
+++ backend/src/handlers_prelude/mod.rs
@@ -1,15 +1,24 @@
 //! All Axum handlers are exported from this module
 
 mod doc;
+use std::collections::HashMap;
+
+use axum::{extract::State, http::HeaderMap};
+use chrono::{DateTime, Utc};
 pub use doc::*;
 mod tree;
 pub use tree::*;
 mod oauth;
 pub use oauth::*;
 
-use color_eyre::Report;
-use log::error;
+use color_eyre::{
+    eyre::{Context, ContextCompat},
+    Report,
+};
+use log::{debug, error, trace};
 use reqwest::StatusCode;
+
+use crate::{db::User, perms::Permission, AppState};
 
 /// Quick and dirty way to convert an eyre error to a (StatusCode, message) response, meant for use with `map_err`, so that errors can be propagated out of
 /// axum handlers with `?`.
@@ -19,4 +28,102 @@ pub fn eyre_to_axum_err(e: Report) -> (StatusCode, String) {
         StatusCode::INTERNAL_SERVER_ERROR,
         format!("An error was encountered, check server logs for more info: {e}"),
     )
+}
+
+/// The output of a find_user call, used to differentiate between expired users and valid users
+enum FoundUser {
+    ExpiredUser(User),
+    User(User),
+}
+
+/// Find the user attached to a particular request, if there is one, and the access token is still valid
+async fn find_user(state: &AppState, headers: HeaderMap) -> color_eyre::Result<Option<FoundUser>> {
+    let mut cookies: HashMap<&str, &str> = HashMap::new();
+    // There can be multiple cookie headers, and each cookie header can contain multiple cookies
+    let cookie_headers = headers.get_all("Cookie");
+    for cookie_header in cookie_headers {
+        let deserialized_cookie = cookie_header
+            .to_str()
+            .wrap_err("Cookie header contains invalid UTF-8")?;
+        for nv_pair in deserialized_cookie.split("; ") {
+            let (name, value) = nv_pair.split_once('=').wrap_err("Malformed cookie")?;
+            cookies.insert(name, value);
+        }
+    }
+    if let Some(token) = cookies.get("access-token") {
+        trace!("Request was made that contains an access-token cookie");
+        if let Some(user) = state.db.get_user_from_token(token.to_string()).await? {
+            let expiration_date = DateTime::parse_from_rfc3339(&user.expiration_date)
+                .wrap_err("Expiration time in database is not a valid time")?;
+            if expiration_date < Utc::now() {
+                debug!("User {:?} made a request that requires a valid access token but their access token expired", user.username);
+                return Ok(Some(FoundUser::ExpiredUser(user)));
+            } else {
+                debug!("User {:?} made a request that requires a valid access token and they have a valid access token", user.username);
+                return Ok(Some(FoundUser::User(user)));
+            }
+        } else {
+            trace!("No user was found in the database with the request's access token");
+        }
+    } else {
+        trace!("Request was made that lacked an access-token cookie");
+    }
+
+    Ok(None)
+}
+
+pub async fn require_perms(
+    State(state): State<&AppState>,
+    headers: HeaderMap,
+    perms: &[Permission],
+) -> Result<User, (StatusCode, String)> {
+    let maybe_user = find_user(state, headers).await.map_err(eyre_to_axum_err)?;
+    match maybe_user {
+        Some(user) => match user {
+            FoundUser::ExpiredUser(u) => Err((
+                StatusCode::UNAUTHORIZED,
+                format!(
+                    "The access token has expired for the user {}, they must authenticate again.",
+                    u.username
+                ),
+            )),
+            FoundUser::User(u) => {
+                let groups = &state
+                    .db
+                    .get_user_groups(u.id)
+                    .await
+                    .map_err(eyre_to_axum_err)?;
+                let mut has_permissions = false;
+                for &perm in perms {
+                    let mut has_permission = false;
+                    for group in groups {
+                        if state
+                            .db
+                            .group_has_permission(group.id, perm)
+                            .await
+                            .map_err(eyre_to_axum_err)?
+                        {
+                            has_permission = true;
+                        }
+                    }
+                    has_permissions = has_permissions && has_permission;
+                }
+                if has_permissions {
+                    Ok(u)
+                } else {
+                    Err((
+                        StatusCode::FORBIDDEN,
+                        format!(
+                            "User {:?} lacks the permission to edit documents.",
+                            u.username
+                        ),
+                    ))
+                }
+            }
+        },
+        None => Err((
+            StatusCode::UNAUTHORIZED,
+            "No valid user is authenticated.".to_string(),
+        )),
+    }
 }
