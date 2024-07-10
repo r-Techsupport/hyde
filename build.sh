cd862854b9e88a2df307e50970df4403540f1464
sealsrock12 <sign@toad39.dev>
2024-07-10 11:21:57 -0700

    change unix build script to use symlinks instead of copying

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
+mkdir -p target
+
 echo "Building the frontend..."
 cd frontend; npm i; npm run build; cd ..
-echo "Copying frontend files..."
-mkdir -p target/web
-cp -r frontend/build/ target/web
+echo "Linking frontend files..." # we are making it a link so you can build the frontend individually without manually copying it if you want
+if ! [ -L target/web ]
+then
+    echo "target/web is not a symlink, removing it"
+    rm -r target/web
+else 
+    unlink target/web
+fi
+ln -s ../frontend/build target/web
 
 echo "Building the backend..."
 cd backend; cargo build --release; cd ..
-echo "Copying backend files..."
-cp backend/target/release/hyde-backend target/hyde
+echo "Linking backend files..."
+rm target/hyde
+ln -s ../backend/target/release/hyde-backend target/hyde
