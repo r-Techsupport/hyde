210dadc22d26822a9d0491862bf04ab159f5e56c
arc <zleyyij@users.noreply.github.com>
2025-01-09 10:56:06 -0700

    fix(frontend): failing build caused by stray troubleshooting

--- frontend/src/lib/components/dashboard/AdminDashboard.svelte
+++ frontend/src/lib/components/dashboard/AdminDashboard.svelte
@@ -6,12 +6,12 @@
 	// import BaseBranch from './BaseBranch.svelte';
 
 	export let dialog: HTMLDialogElement;
-	let selectedTab = 3;
+	let selectedTab = 0;
 	let tabs = [
 		{ name: 'User Management', id: 0, component: UserTab },
 		{ name: 'Group Management', id: 1, component: GroupTab },
-		{ name: 'Server Management', id: 2, component: ServerTab }
-		// { name: 'Branch Management', id: 3, component: BranchTab },
+		{ name: 'Server Management', id: 2, component: ServerTab },
+ 		// { name: 'Branch Management', id: 3, component: BranchTab },
 		// { name: 'Base Branch', id: 4, component: BaseBranch }
 	];
 
