47f3c5221915dee4ea789a792f3792e175cb7b22
arc <zleyyij@users.noreply.github.com>
2025-01-10 10:18:14 -0700

    refactor(frontend): more svelte 5 stuff

---
+++ frontend/src/lib/components/dashboard/BranchTab.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+	import { baseBranch, allBranches, me } from '$lib/main'; // Import your store with all branches and user data
+	import { Permission } from '$lib/types';
+	import SectionHeader from '../elements/SectionHeader.svelte';
+	import ToggleSwitch from '../elements/ToggleSwitch.svelte';
+
+	// Check if the user has the ManageBranches permission
+	const canAccess = $me.permissions?.includes(Permission.ManageBranches);
+	const sortedBranches = $state($allBranches.slice().sort((a, b) => a.name.localeCompare(b.name)));
+</script>
+
+{#if canAccess}
+	<div class="container">
+		<SectionHeader --scale="0.9rem" --text-color="var(--foreground-2)">Pull Requests</SectionHeader>
+		<div class="base-branch-container">
+			<label for="base-branch">Destination</label>
+			<select name="base-branch" bind:value={$baseBranch} class="branch-dropdown">
+				{#each $allBranches as { name }}
+					<option value={name}>{name}</option>
+				{/each}
+			</select>
+			<p>All pull requests opened by Hyde will pull into this branch.</p>
+		</div>
+		<ul>
+			<SectionHeader --scale="0.9rem">Enable Branch Protection For:</SectionHeader>
+			{#each sortedBranches as branch}
+				<li>
+					<!-- <label class="checkbox-label">
+						<input type="checkbox" bind:checked={branch.isProtected} />
+						{branch.name}
+					</label> -->
+					<ToggleSwitch --size="0.97rem" bind:checked={branch.isProtected}
+						>{branch.name}</ToggleSwitch
+					>
+				</li>
+			{/each}
+		</ul>
+	</div>
+{:else}
+	<p>You do not have permission to view this page.</p>
+{/if}
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-height: 100%;
+		overflow-y: scroll;
+		justify-items: center;
+		padding: 0.5rem 1rem;
+	}
+
+	.container ul {
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	.container li {
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		margin-top: 0.3rem;
+	}
+
+	.base-branch-container {
+		display: flex;
+		flex-direction: column;
+		margin: 0.3rem;
+	}
+
+	.base-branch-container label {
+		margin-left: 0.1rem;
+		font-size: 0.8rem;
+		color: var(--foreground-3);
+		width: 100%;
+	}
+
+	.base-branch-container p {
+		color: var(--foreground-4);
+		font-size: 0.6rem;
+		margin: 0.1rem;
+		margin-left: 0.3rem;
+	}
+
+	.branch-dropdown {
+		font-size: 1rem;
+		padding: 0.25rem;
+		height: 2rem;
+		margin: 0.1rem;
+		width: 100%;
+		max-width: 20rem;
+		border-radius: 3px;
+		background-color: var(--background-1);
+		color: var(--foreground-0);
+	}
+
+	.branch-dropdown option {
+		padding: 0.625rem;
+	}
+</style>
---
+++ frontend/src/lib/components/elements/ToggleSwitch.svelte
@@ -0,0 +1,96 @@
+<!-- https://svelte.dev/playground/d65a4e9f0ae74d1eb1b08d13e428af32?version=3.35.0 -->
+
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	interface Props {
+		size?: number;
+		checked?: boolean;
+		onToggle?: (checked: boolean) => undefined;
+		children?: Snippet;
+	}
+
+	let {
+		size = 1,
+		checked = $bindable(true),
+		onToggle = () => {},
+		children
+	}: Props = $props();
+
+	const uniqueID = Math.floor(Math.random() * 100);
+
+	function handleClick(event: MouseEvent) {
+		const target: HTMLInputElement = event.target! as HTMLInputElement;
+		const state = target.getAttribute('aria-checked');
+		checked = state !== 'true';
+		onToggle(checked);
+	}
+</script>
+
+<div class="toggle-switch" style="--size={size}">
+	<span>{@render children?.()}</span>
+	<button
+		role="switch"
+		aria-checked={checked}
+		aria-labelledby={`switch-${uniqueID}`}
+		onclick={handleClick}
+		style="--size={size}"
+	>
+	</button>
+</div>
+
+<style>
+	.toggle-switch {
+		display: flex;
+		box-sizing: border-box;
+		align-items: center;
+		width: 100%;
+	}
+
+	.toggle-switch span {
+		margin-left: calc(var(--size) / 4);
+		font-size: var(--size);
+		text-overflow: ellipsis;
+		overflow: hidden;
+		max-width: calc(100% - var(--size) * 2.3);
+	}
+
+	.toggle-switch button {
+		flex-shrink: 0;
+		width: calc(var(--size) * 2.3);
+		height: calc(var(--size) * 1.1);
+		background: var(--background-3);
+
+		/* margin: 0 0 0 calc(var(--size)); */
+		position: relative;
+		border: none;
+		cursor: pointer;
+		margin-left: auto;
+		border-radius: var(--size);
+	}
+
+	.toggle-switch button::before {
+		content: '';
+		position: absolute;
+		width: var(--size);
+		height: var(--size);
+		background: var(--foreground-3);
+
+		/* top: 0.15em; */
+
+		/* right: 0.5em; */
+		top: calc(var(--size) / 20);
+		right: calc(var(--size) * 1.2);
+		transition: transform 0.2s;
+		border-radius: 100%;
+	}
+
+	.toggle-switch button[aria-checked='true'] {
+		background-color: var(--accent-0);
+	}
+
+	.toggle-switch button[aria-checked='true']::before {
+		transform: translateX(calc(var(--size) / 0.9));
+		transition: transform 0.2s;
+		background-color: var(--foreground-1);
+	}
+</style>
