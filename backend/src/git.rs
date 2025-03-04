a17de32aba328a1bed1567674817485b6bec0c45
arc <zleyyij@users.noreply.github.com>
2025-03-04 09:54:54 -0700

    fix(backend): checkout now correctly starts from parent branch?

--- backend/src/git.rs
+++ backend/src/git.rs
@@ -390,6 +390,7 @@ impl Interface {
             let parent_branch = repo
                 .find_branch(parent_branch_name, BranchType::Local)
                 .wrap_err("Failed to locate the parent branch")?;
+            repo.set_head(&format!("refs/heads/{}", parent_branch_name))?;
 
             // Check if the branch already exists
             match repo.find_branch(branch_name, BranchType::Local) {
