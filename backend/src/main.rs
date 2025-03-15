fb84e2bea72b14712be80614ba7233335f0eefd7
TheKrol <TheKrol@users.noreply.github.com>
2025-03-14 23:21:48 -0400

    Image fix (#406)
    * Asset fix
    
    * format update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -206,7 +206,7 @@ async fn start_server(state: AppState, cli_args: Args) -> Result<()> {
         .with_state(state)
         // Serve the assets folder from the repo
         .nest_service(
-            &format!("/{asset_path}"),
+            "/assets",
             ServeDir::new(format!("repo/{asset_path}")),
         )
         // Serve the frontend files
