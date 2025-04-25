552150f980888dd0dea06b2af95c8131116f2b83
TheKrol <TheKrol@users.noreply.github.com>
2025-04-25 01:47:44 -0400

    Fix multipage document reseting

--- backend/src/git.rs
+++ backend/src/git.rs
@@ -390,13 +390,9 @@ impl Interface {
             let parent_branch = repo
                 .find_branch(parent_branch_name, BranchType::Local)
                 .wrap_err("Failed to locate the parent branch")?;
-            repo.set_head(&format!("refs/heads/{}", parent_branch_name))?;
+            // repo.set_head(&format!("refs/heads/{}", parent_branch_name))?;
 
-            repo.reset(
-                &parent_branch.get().peel(git2::ObjectType::Commit)?,
-                git2::ResetType::Hard,
-                None,
-            )?;
+            
 
             // Check if the branch already exists
             match repo.find_branch(branch_name, BranchType::Local) {
@@ -431,6 +427,11 @@ impl Interface {
                         .wrap_err_with(|| {
                             format!("Failed to set HEAD to new branch {}", branch_name)
                         })?;
+                    repo.reset(
+                        &parent_branch.get().peel(git2::ObjectType::Commit)?,
+                        git2::ResetType::Hard,
+                        None,
+                    )?;
                     repo.checkout_head(None)?;
                 }
             }
