48d24dc7ab48007fe8c1e0a68ffe1e1751fcaada
Koei <205915319+Koei32@users.noreply.github.com>
2026-05-28 22:21:23 +0530

    User search in admin panel (#758)
    * feat: add basic user search in admin panel
    
    * plus tiny style tweaks
    
    * feat: message when no users found in search
    
    * style: make linter happy
    
    * style: make linter happy again
    
    * refactor: remove unnecessary `if`
    
    * refactor: move search logic from handler to `$derived()`

--- frontend/src/lib/components/dashboard/UserTab.svelte
+++ frontend/src/lib/components/dashboard/UserTab.svelte
@@ -53,6 +53,11 @@
 		}
 	}
 
+	let searchQuery = $state('');
+	let displayedUsers: User[] = $derived(
+		users.filter((user) => user.username.includes(searchQuery.trim().toLowerCase()))
+	);
+
 	onMount(async () => {
 		allGroups = await (await fetch(`${apiAddress}/api/groups`, { credentials: 'include' })).json();
 		users = await (await fetch(`${apiAddress}/api/users`, { credentials: 'include' })).json();
@@ -70,24 +75,20 @@
 <div class="container">
 	<ul class="user-menu">
 		<SectionHeader>Users</SectionHeader>
+		<input class="search" type="text" placeholder="Search users" bind:value={searchQuery} />
 		{#each users.entries() as [index, user] (index)}
-			<li class={selectedUser == index ? 'selected-user' : ''} id={index.toString()}>
-				<button onclick={userSelectHandler}>
-					<!-- <svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24px"
-						viewBox="0 -960 960 960"
-						width="24px"
-					>
-						<path
-							d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"
-						/>
-					</svg> -->
-					<img src={user.avatar_url} alt="User avatar" width="25rem" height="25rem" />
-					<span>{user.username}</span>
-				</button>
-			</li>
+			{#if displayedUsers.includes(user)}
+				<li class={selectedUser == index ? 'selected-user' : ''} id={index.toString()}>
+					<button onclick={userSelectHandler}>
+						<img src={user.avatar_url} alt="User avatar" width="25rem" height="25rem" />
+						<span>{user.username}</span>
+					</button>
+				</li>
+			{/if}
 		{/each}
+		{#if displayedUsers.length == 0}
+			<span>No users found.</span>
+		{/if}
 	</ul>
 	<ul class="group-menu">
 		<SectionHeader>Groups</SectionHeader>
@@ -117,22 +118,39 @@
 	.user-menu {
 		margin-top: 0;
 		margin-bottom: 0;
-		padding-left: 0;
+		padding: 0 0.2rem;
 		height: 100%;
 		border-right: 1.5px solid var(--background-3);
 		list-style-type: none;
 		width: 50%;
-		overflow-y: scroll;
+		overflow-y: auto;
 	}
 
 	.group-menu {
 		margin-top: 0;
 		margin-bottom: 0;
-		padding-left: 0;
+		padding: 0 0.2rem;
 		list-style-type: none;
 		width: 50%;
 		height: 100%;
 		overflow-y: scroll;
+	}
+
+	.search {
+		padding: 0;
+		margin-bottom: 0.4rem;
+		color: var(--foreground-3);
+		background-color: var(--background-3);
+		border: none;
+		width: 100%;
+		height: 1.75rem;
+		border-radius: 0.25rem;
+		text-indent: 1rem;
+	}
+
+	.search:focus {
+		outline: none;
+		border-bottom: 1px solid var(--foreground-3);
 	}
 
 	.user-menu img {
